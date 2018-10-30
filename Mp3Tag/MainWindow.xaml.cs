3c411bdecce0bd955f44fdd21e98be4acb38d135
MERTENS, Matthias <m.mertens@evs.com>
2018-10-30 19:10:50 +0100

    Move song to a file better regex with named groups

--- Mp3Tag/MainWindow.xaml.cs
+++ Mp3Tag/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace Mp3Tag
 {
-  public class Song
-  {
-    public string Album { get; set; }
-    public string Folder { get; set; }
-    public string Number { get; set; }
-    public string Artist { get; set; }
-    public string Title { get; set; }
-    public string Path { get; set; }
-    public string Thumbnail { get; set; }
-  }
-
-  // Regex V1
-  // [0-9]{4}-[0-9]{2}\\CD [0-9]\\[0-9]{2} - [a-zA-Z0-9-(.)]+ - [a-zA-Z0-9 (.)]+
 
   /// <summary>
   /// Interaction logic for MainWindow.xaml
@@ -31,7 +20,12 @@ namespace Mp3Tag
 
     public List<Song> GetAllSongs()
     {
-      var files = Directory.GetFiles(@"C:\Users\mme\Desktop\dev-dir", "*.mp3", SearchOption.AllDirectories);
+      var files = Directory.GetFiles(@"D:\Matthias\Musique\Compilations", "*.mp3", SearchOption.AllDirectories);
+      var toto = new Regex(@"(?<date>\[0-9\]{4}-\[0-9\]{2})\(?<cd>CD \[0-9\])\(?<number>\[0-9\]{2}) - (?<artist>\[a-zA-Z0-9-(.)\]+) - (?<title>\[a-zA-Z0-9 (.)'\]+)");
+      foreach(var file in files)
+      {
+        var regexResponses = toto.Match(file);
+      }
       return null;
     }
 
---
+++ Mp3Tag/Song.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Mp3Tag
+{
+  public class Song
+  {
+    public string Album { get; set; }
+    public string Folder { get; set; }
+    public string Number { get; set; }
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Path { get; set; }
+    public string Thumbnail { get; set; }
+  }
+}
