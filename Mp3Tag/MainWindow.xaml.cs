b734606c676a56777ac0fd5d4dfb11a00e6d5b0f
Matthias Mertens <matthias0212@gmail.com>
2018-10-30 13:45:06 +0100

    First commit solution

---
+++ Mp3Tag/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace Mp3Tag
+{
+  public class Song
+  {
+    public string Album { get; set; }
+    public string Folder { get; set; }
+    public string Number { get; set; }
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Path { get; set; }
+    public string Thumbnail { get; set; }
+  }
+
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      GetAllSongs();
+    }
+
+    public List<Song> GetAllSongs()
+    {
+      var files = Directory.GetFiles(@"C:\Users\mme\Desktop\dev-dir", "*.mp3", SearchOption.AllDirectories);
+      return null;
+    }
+
+    public void Toto()
+    {
+      //TagLib.
+    }
+  }
+}
