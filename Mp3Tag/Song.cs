98d9b34401cdc68b88bb75af59faae1bee95e002
MERTENS, Matthias <m.mertens@evs.com>
2018-11-06 13:45:37 +0100

    Add taglib, open folder, show path in grid, open first tags

--- Mp3Tag/Song.cs
+++ Mp3Tag/Song.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows;
+using TagLib;
 
 namespace Mp3Tag
 {
   public class Song
   {
-    public string Album { get; set; }
-    public string Folder { get; set; }
+    public string Date { get; set; }
+    public string Cd { get; set; }
     public string Number { get; set; }
     public string Artist { get; set; }
     public string Title { get; set; }
     public string Path { get; set; }
-    public string Thumbnail { get; set; }
     public bool IsInError { get; set; }
+    public string TagArtist { get;set; }
+    public string TagAlbum { get; set; }
+    public Bitmap TagThumbnail { get; set; }
+    public string TagTitle { get; set; }
+    public string TagNumber { get; set; }
+    public string TagCd { get; set; }
   }
 }
--- Mp3Tag/ViewModel/MainViewModel.cs
+++ Mp3Tag/ViewModel/MainViewModel.cs
@@ -1,57 +1,97 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using Microsoft.Win32;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using System.Windows.Input;
+using TagLib;
+using TagLib.Mpeg;
 
 namespace Mp3Tag.ViewModel
 {
   public class MainViewModel : ViewModelBase
   {
     private string _workingDirectoryPath;
+    private ObservableCollection<Song> _songs;
 
     public MainViewModel()
     {
-      TOtoCommand = new RelayCommand(OpenFileDialog);
+      OpenWorkingDirectoryCommand = new RelayCommand(OpenWorkingDirectory);
+      LoadWorkingDirectoryCommand = new RelayCommand(LoadWorkingDirectory, !string.IsNullOrEmpty(WorkingDirectoryPath));
+
+      WorkingDirectoryPath = @"C:\Users\mme\Desktop\dev-dir";
+      LoadWorkingDirectory();
     }
 
-    private void OpenFileDialog()
+    private void OpenWorkingDirectory()
     {
-      OpenFileDialog openFileDialog = new OpenFileDialog();
-      if (openFileDialog.ShowDialog() == true)
-        WorkingDirectoryPath = File.ReadAllText(openFileDialog.FileName);
+      FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+      if (openFileDialog.ShowDialog() == DialogResult.OK)
+        WorkingDirectoryPath = openFileDialog.SelectedPath;
+    }
+
+    private void LoadWorkingDirectory()
+    {
+      Songs = GetAllSongs();
     }
 
     public string WorkingDirectoryPath
     {
       get { return _workingDirectoryPath; }
-      set { _workingDirectoryPath = value;
+      set
+      {
+        _workingDirectoryPath = value;
         RaisePropertyChanged();
       }
     }
-    public ICommand TOtoCommand { get; set; }
 
-    public List<Song> GetAllSongs()
+    public ObservableCollection<Song> Songs
     {
-      var songs = new List<Song>();
-      //var trackFiles = Directory.GetFiles(@"C:\Users\mme\Desktop\dev-dir", "*.mp3", SearchOption.AllDirectories);
-      var trackFiles = Directory.GetFiles(@"D:\Matthias\Musique\Compilations", "*.mp3", SearchOption.AllDirectories);
+      get { return _songs; }
+      set
+      {
+        _songs = value;
+        RaisePropertyChanged();
+      }
+    }
+
+    public ICommand OpenWorkingDirectoryCommand { get; set; }
+    public ICommand LoadWorkingDirectoryCommand { get; set; }
+
+    public ObservableCollection<Song> GetAllSongs()
+    {
+      var songs = new ObservableCollection<Song>();
+      var trackFiles = Directory.GetFiles(WorkingDirectoryPath, "*.mp3", SearchOption.AllDirectories);
+      //var trackFiles = Directory.GetFiles(@"D:\Matthias\Musique\Compilations", "*.mp3", SearchOption.AllDirectories);
       var regex = new Regex(@"(?<date>[0-9]{4}-[0-9]{2})\\(?<cd>CD [0-9])\\(?<number>[0-9]{2}) - (?<artist>[a-zA-Z0-9-(.) ]+) - (?<title>[a-zA-Z0-9 (.)']+)\.mp3");
       foreach (var file in trackFiles)
       {
+        AudioFile audioFile = (AudioFile)TagLib.File.Create(file);
+        audioFile.Tag.Pictures = new IPicture[] { new Picture(@"C:\Users\mme\Desktop\téléchargement.png"), };
+
+        var img = new Bitmap(@"C:\Users\mme\Desktop\téléchargement.png");
+
+
         var match = regex.Match(file);
         if (match.Success)
         {
           songs.Add(new Song
           {
             Artist = match.Groups["artist"].Value,
-            Album = match.Groups["date"].Value,
-            Folder = match.Groups["cd"].Value,
+            Date = match.Groups["date"].Value,
+            Cd = match.Groups["cd"].Value,
             Number = match.Groups["number"].Value,
             Path = file,
-            Title = match.Groups["title"].Value
+            Title = match.Groups["title"].Value,
+            TagAlbum = audioFile.Tag.Album,
+            TagArtist = audioFile.Tag.Performers.FirstOrDefault(),
+            TagCd = audioFile.Tag.Disc.ToString(),
+            TagNumber = audioFile.Tag.Track.ToString(),
+            TagThumbnail = img,
+            TagTitle = audioFile.Tag.Title
           });
         }
         else
